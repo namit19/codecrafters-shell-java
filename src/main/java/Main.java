852ac674c1e38ce481ea839e647cdef642aceb99
Namit Sangoi <namit.25bcs10438@sst.scaler.com>
2026-06-20 14:58:31 +0530

    Pass stage X

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,10 +15,24 @@ public class Main {
 
             if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
-            } else {
-                String command = input.split(" ")[0];
-                System.out.println(command + ": command not found");
+                continue;
             }
+
+            if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (command.equals("echo") ||
+                    command.equals("exit") ||
+                    command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+                continue;
+            }
+
+            String command = input.split(" ")[0];
+            System.out.println(command + ": command not found");
         }
     }
 }
