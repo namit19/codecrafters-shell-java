262a03072d1d531a0005e00509835d87caed6e48
Namit Sangoi <namit.25bcs10438@sst.scaler.com>
2026-06-20 15:29:40 +0530

    Pass stage X

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,40 @@
-private static List<String> parseCommand(String input) {
-    List<String> args = new ArrayList<>();
-    StringBuilder current = new StringBuilder();
+import java.util.ArrayList;
+import java.util.List;
 
-    boolean inSingleQuotes = false;
+public class Main {
 
-    for (int i = 0; i < input.length(); i++) {
-        char ch = input.charAt(i);
+    private static List<String> parseCommand(String input) {
+        List<String> args = new ArrayList<>();
+        StringBuilder current = new StringBuilder();
 
-        if (ch == '\'') {
-            inSingleQuotes = !inSingleQuotes;
-            continue;
-        }
+        boolean inSingleQuotes = false;
 
-        if (Character.isWhitespace(ch) && !inSingleQuotes) {
-            if (current.length() > 0) {
-                args.add(current.toString());
-                current.setLength(0);
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch == '\'') {
+                inSingleQuotes = !inSingleQuotes;
+                continue;
             }
-        } else {
-            current.append(ch);
+
+            if (Character.isWhitespace(ch) && !inSingleQuotes) {
+                if (current.length() > 0) {
+                    args.add(current.toString());
+                    current.setLength(0);
+                }
+            } else {
+                current.append(ch);
+            }
         }
+
+        if (current.length() > 0) {
+            args.add(current.toString());
+        }
+
+        return args;
     }
 
-    if (current.length() > 0) {
-        args.add(current.toString());
+    public static void main(String[] args) throws Exception {
+        // your shell code here
     }
-
-    return args;
 }
